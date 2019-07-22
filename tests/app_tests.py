182a1c68020f3ab08f42a4e12f2a26eaa43bc886
Evgeny Yakimov <john.yakimov@gmail.com>
2019-07-22 19:00:29 +0100

    fail test

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -3,17 +3,20 @@ import uuid
 
 import app
 
-# Helper
 
+# Helper
 def bytes_to_str(b):
     return ''.join(chr(x) for x in (b))
+
 
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
         self.client = app.app.test_client()
 
-
     def test_uuid_generated(self):
         resp = self.client.get('/')
         assert uuid.UUID(bytes_to_str(resp.data))
+
+    def test_fail_test(self):
+        assert False
